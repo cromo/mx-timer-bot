59727e8a506d0761d9275cf64ee1def152ea056b
Cristián Romo <cristian.g.romo@gmail.com>
2019-10-25 18:32:32 -0500

    Add timer persistence
    This will allow timers to survive across restarts.

--- index.ts
+++ index.ts
@@ -1,19 +1,45 @@
 import { MatrixClient, SimpleFsStorageProvider, AutojoinRoomsMixin } from "matrix-bot-sdk";
 import * as TOML from "@iarna/toml";
 import { readFileSync } from "fs";
+import * as Database from "better-sqlite3";
 
 interface Config {
   homeserverUrl: string;
   accessToken: string;
-  storage?: string;
+  syncFile?: string;
+  dbFile?: string;
 };
 
 const config = getConfig();
+const db = new Database(config.dbFile);
 
-const client = new MatrixClient(config.homeserverUrl, config.accessToken, new SimpleFsStorageProvider(config.storage));
+db.exec(`
+create table if not exists timers (
+    eventId text primary key not null,
+    roomId text not null,
+    time text not null,
+    message text not null
+)`);
+
+const client = new MatrixClient(config.homeserverUrl, config.accessToken, new SimpleFsStorageProvider(config.syncFile));
 AutojoinRoomsMixin.setupOnClient(client);
 
-client.start().then(() => console.log("Client started"));
+client.start().then(() => {
+    console.log("Client started");
+    const existingTimers = db.prepare(`
+        select eventId, roomId, time, message
+        from timers
+    `).all().map(t => ({...t, time: new Date(t.time)}));
+
+    const now = new Date();
+    const remaining = existingTimers.filter(t => now < t.time);
+    remaining.forEach(t => setTimeout(() => sendReminder(t.roomId, t.message, t.eventId), t.time - +now));
+    console.log(`Restarted ${remaining.length} timers`);
+
+    const pastDue = existingTimers.filter(t => t.time <= now);
+    pastDue.forEach(t => sendReminder(t.roomId, t.message, t.eventId));
+    console.log(`Sent notifications for ${pastDue.length} past due timers`);
+});
 client.on('room.message', (roomId, event) => {
     if (![hasContent, startsWithBangCommand("timer")].every(f => f(event))) {
         return;
@@ -21,17 +47,24 @@ client.on('room.message', (roomId, event) => {
     const timer = extractDurationAndMessage(event.content.body.substring("!timer".length).trim());
     if (!timer) return client.sendNotice(roomId, "Unrecognized format");
     const [delay, message] = timer;
-    setTimeout(() => {
-        client.sendNotice(roomId, message);
-    }, delay);
+    const changes = db.prepare('insert into timers (eventId, roomId, time, message) values (?, ?, ?, ?)').run(event.event_id, roomId, new Date(+new Date() + delay).toISOString(), message);
+    console.log("Database rows added", changes);
+    setTimeout(() => sendReminder(roomId, message, event.event_id), delay);
 });
+
+function sendReminder(roomId: string, message: string, eventId: string) {
+    client.sendNotice(roomId, message);
+    const deletions = db.prepare(`delete from timers where eventId = ?`).run(eventId);
+    console.log("Database rows deleted", deletions);
+}
 
 function getConfig(): Config {
     const fileConfig = TOML.parse(readFileSync("config.toml", {encoding: "utf-8"})) as unknown as Config;
     return {
         homeserverUrl: process.env.MX_TIMER_BOT_HOMESERVER_URL || fileConfig.homeserverUrl || "https://matrix.org",
         accessToken: process.env.MX_TIMER_BOT_ACCESS_TOKEN || fileConfig.accessToken || "TOKEN NOT SET",
-        storage: process.env.MX_TIMER_BOT_SYNC_FILE || fileConfig.storage || "sync.json"
+        syncFile: process.env.MX_TIMER_BOT_SYNC_FILE || fileConfig.syncFile || "sync.json",
+        dbFile: process.env.MX_TIMER_BOT_DB_FILE || fileConfig.dbFile || "timers.db"
     } as Config;
 }
 
