27bf2c8efbfd471db9156ac117f0fa034dc767fe
Cristián Romo <cristian.g.romo@gmail.com>
2019-10-25 15:36:33 -0500

    Add lookup tables to enable uniform unit handling

--- index.ts
+++ index.ts
@@ -35,12 +35,49 @@ function startsWithBangCommand(command: string): (event: any) => boolean {
     return event => event.content.body.startsWith(`!${command}`);
 }
 
+const unitNormalizations: Map<string, string> = new Map([
+    ["m", "minutes"],
+    ["min", "minutes"],
+    ["mins", "minutes"],
+    ["minute", "minutes"],
+    ["minutes", "minutes"],
+    ["s", "seconds"],
+    ["sec", "seconds"],
+    ["secs", "seconds"],
+    ["second", "seconds"],
+    ["seconds", "seconds"],
+    ["h", "hours"],
+    ["hr", "hours"],
+    ["hrs", "hours"],
+    ["hour", "hours"],
+    ["hours", "hours"],
+    ["d", "days"],
+    ["day", "days"],
+    ["days", "days"],
+    ["w", "weeks"],
+    ["wk", "weeks"],
+    ["wks", "weeks"],
+    ["week", "weeks"],
+    ["weeks", "weeks"]
+]);
+const durationMultipliers: Map<string, number> = new Map([
+    ["seconds", 1000],
+    ["minutes", 60 * 1000],
+    ["hours", 60 * 60 * 1000],
+    ["days", 24 * 60 * 60 * 1000],
+    ["weeks", 7 * 24 * 60 * 60 * 1000]
+]);
+
 function extractDurationAndMessage(str: string): [number, string] | undefined {
     const match = /^(\d+) ?(m|s)?(?:\s+(.*))?$/.exec(str);
     if (!match) return undefined;
-    let [, quantity, unit, message] = match;
-    message = message || `Timer set ${quantity}${unit} ago finished.`;
-    if (unit === 'm') return [+quantity * 60 * 1000, message];
-    if (unit === 's') return [+quantity * 1000, message];
-    return [+quantity * 60 * 1000, message];
+
+    let [, quantityStr, unit, message] = match;
+    const quantity = +quantityStr;
+    unit = unitNormalizations.get(unit);
+    // Should pass in default message instead since it has access to more
+    // information from the event.
+    message = message || `Timer set ${quantity} ${unit} ago finished.`;
+
+    return [quantity * durationMultipliers.get(unit), message];
 }
